f91d6cb8c3be349aa13c71061772e3ac4736c2b9
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 01:20:56 +0300

    add auth middleware

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,27 @@
+'use strict';
+const ApiError = require('../exceptions/api.error');
+const tokenService = require('../service/token-service');
+
+module.exports = function(req, res, next) {
+  try {
+    const authrorizationHeader = req.headers.authorization;
+    if (!authrorizationHeader) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const accessToken = authrorizationHeader.split(' ')[1];
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());
+    }
+
+    req.user =  userData;
+    next();
+  } catch (e) {
+    return next(ApiError.UnauthorizedError());
+  }
+};
