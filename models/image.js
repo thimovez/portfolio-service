1e82f9a55de90fb8518d29f1fbc05be2f880d508
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 00:40:39 +0300

    add model image

---
+++ models/image.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Image.belongsTo(models.Portfolio);
+    }
+  }
+  Image.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    comments: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'images',
+    modelName: 'Image',
+  });
+  return Image;
+};
