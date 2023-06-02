a50afc2280fb69b15be17f06c1bd78719745fbe5
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 00:36:01 +0300

    add image migration

---
+++ migrations/20230602213223-Image.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      comments: {
+        type: Sequelize.STRING,
+        allowNull: true
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    queryInterface.dropTable('images');
+  }
+};
