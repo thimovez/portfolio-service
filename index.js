e488f725fd78beb6738721db4616077b55f9cd60
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 01:19:27 +0300

    add expressjs server config

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const { sequelize } = require('./models');
+const router = require('./router/index');
+const errorMiddleware = require('./middleware/error-middleware');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}/`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+
+start();
