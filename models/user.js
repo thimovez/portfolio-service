2fe11bc417a95bf89c9a87a3de3cf9c3b1f80c71
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 00:45:05 +0300

    remove unused assciate from user model

--- models/user.js
+++ models/user.js
@@ -12,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasOne(models.Token, { foreignKey: 'userId' });
-      User.hasMany(models.Comment, { foreignKey: 'userId' });
       User.hasMany(models.Portfolio, { foreignKey: 'UserId' });
     }
   }
