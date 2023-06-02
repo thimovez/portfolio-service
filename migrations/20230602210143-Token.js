69625e913235b2f62796f121f0b88733e7119d01
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 00:14:52 +0300

    add token migration

---
+++ migrations/20230602210143-Token.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      refreshToken: {
+        type: Sequelize.STRING(260)
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    queryInterface.dropTable('tokens');
+  }
+};
