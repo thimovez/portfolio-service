d4687c8b28c7b03dead9bb6ae595a6fe4dbc5b30
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 20:56:58 +0300

    add controller upload image by id

--- controllers/portfolio-controller.js
+++ controllers/portfolio-controller.js
@@ -6,10 +6,25 @@ class PortfolioController {
     try {
       const { id } = req.user;
       const images = req.files || '';
+      const args = req.body;
 
-      const p = await portfolioService.createPortfolio(id, images, req.body);
+      const p = await portfolioService.createPortfolio(id, images, args);
 
       return res.json(p);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async UploadImagesByID(req, res, next) {
+    try {
+      const id = req.params.id;
+      const images = req.files || '';
+      const args = req.body;
+
+      const imgData = await portfolioService.UploadImagesByID(id, images, args);
+
+      return res.json(imgData);
     } catch (e) {
       next(e);
     }
