02db8375a0912f92d441eab27799a00791e1bbf1
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 16:31:09 +0300

    add multer middleware to create portfolio

--- router/portfolio-router.js
+++ router/portfolio-router.js
@@ -1,9 +1,12 @@
 'use strict';
 const Router = require('express');
+const multer  = require('multer');
 const portfolioController = require('../controllers/portfolio-controller');
 const auth = require('../middleware/auth-middleware');
 const router = new Router();
+const upload = multer({ dest: 'uploads/' });
 
-router.post('/', auth, portfolioController.createPortfolio);
+router.post('/', auth, upload.single('image'),
+  portfolioController.createPortfolio);
 
 module.exports = router;
