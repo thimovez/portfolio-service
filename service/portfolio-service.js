9358992dfa7d415ea6361ef9b27e08c01cfef51d
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-03 20:57:39 +0300

    decompose logic of upload images

--- service/portfolio-service.js
+++ service/portfolio-service.js
@@ -3,14 +3,8 @@ const ApiError = require('../exceptions/api.error');
 const { Portfolio, Image } = require('../models');
 
 class PortfolioService {
-  async createPortfolio(id, images, ...args) {
-    const {
-      name,
-      description = '',
-      imgname = '',
-      imgdescr = '',
-      imgcomment = ''
-    } = args[0];
+  async createPortfolio(id, images, args) {
+    const { name, description = '' } = args;
 
     if (typeof name === 'undefined') {
       throw ApiError.BadRequest('field name cannot be empty');
@@ -20,6 +14,26 @@ class PortfolioService {
       name, description, UserId: id
     });
 
+    const imagesData = await this.UploadImages(portfolioData.id, images, args);
+
+    return {
+      portfolioData,
+      imagesData
+    };
+  }
+
+  async UploadImagesByID(id, images, args) {
+    if (typeof id === 'undefined') {
+      throw ApiError.BadRequest('missed portfoio id');
+    }
+
+    const imagesData = this.UploadImages(id, images, args);
+
+    return imagesData;
+  }
+
+  async UploadImages(id, images, args) {
+    const { imgname = '', imgdescr = '', imgcomment = '' } = args;
     const imagesData = {};
 
     for (let i = 0; i < images.length; i++) {
@@ -27,17 +41,14 @@ class PortfolioService {
         name: imgname,
         description: imgdescr,
         comments: imgcomment,
-        PortfolioId: portfolioData.id,
+        PortfolioId: id,
         path: images[i].path
       });
 
       imagesData[i] = image;
     }
 
-    return {
-      portfolioData,
-      imagesData
-    };
+    return imagesData;
   }
 }
 
