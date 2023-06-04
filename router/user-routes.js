8cf3ef0a634124b2e70325648773eb23eadb4914
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-04 19:23:03 +0300

    implement rules to user routers

--- router/user-routes.js
+++ router/user-routes.js
@@ -2,12 +2,21 @@
 const Router = require('express');
 const userController = require('../controllers/user-controller');
 const auth = require('../middleware/auth-middleware');
+const {
+  regUserRules,
+  loginUserRules,
+  logoutUserRules } = require('../utils/validate-rules');
+const validate = require('../middleware/validation-middleware');
 const router = new Router();
 
-router.post('/registration', userController.registrarion);
-router.post('/login', userController.login);
-router.get('/logout', userController.logout);
-router.get('/refresh', userController.refresh);
+router.post('/registration',
+  regUserRules, validate, userController.registrarion);
+router.post('/login',
+  loginUserRules, validate, userController.login);
+router.get('/logout',
+  logoutUserRules, validate, userController.logout);
+router.get('/refresh',
+  logoutUserRules, validate, userController.refresh);
 router.delete('/delete', auth, userController.delete);
 
 module.exports = router;
