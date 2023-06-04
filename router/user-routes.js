d909d8189a674b73f78e11459f0d0e418e8fac1e
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-04 23:35:22 +0300

    refactor

--- router/user-routes.js
+++ router/user-routes.js
@@ -4,8 +4,7 @@ const userController = require('../controllers/user-controller');
 const auth = require('../middleware/auth-middleware');
 const {
   regUserRules,
-  loginUserRules,
-  logoutUserRules } = require('../utils/validate-rules');
+  loginUserRules } = require('../utils/validate-rules');
 const validate = require('../middleware/validation-middleware');
 const router = new Router();
 
@@ -13,10 +12,8 @@ router.post('/registration',
   regUserRules, validate, userController.registrarion);
 router.post('/login',
   loginUserRules, validate, userController.login);
-router.get('/logout',
-  logoutUserRules, validate, userController.logout);
-router.get('/refresh',
-  logoutUserRules, validate, userController.refresh);
+router.get('/logout', validate, userController.logout);
+router.get('/refresh', validate, userController.refresh);
 router.delete('/delete', auth, userController.delete);
 
 module.exports = router;
