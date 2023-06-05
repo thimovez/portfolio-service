70995f1ee70a2daeac514c17d9b2a1489d18b042
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-06-05 16:02:23 +0300

    remove swagger comments

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
 const { sequelize } = require('./models');
 const router = require('./router/index');
 const errorMiddleware = require('./middleware/error-middleware');
@@ -11,7 +9,6 @@ const errorMiddleware = require('./middleware/error-middleware');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', router);
